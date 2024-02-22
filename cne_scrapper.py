74e7e9828288746ffabc76e3fbde481f27f44b90
Jose Valerio <pioxscorpio@gmail.com>
2024-02-22 18:48:07 -0400

    Code cleaning

--- cne_scrapper.py
+++ cne_scrapper.py
@@ -7,6 +7,6 @@ def get_name(nacionalidad: str, ci: str) -> str:
 		web = urllib.request.urlopen(f'http://www.cne.gob.ve/web/registro_electoral/ce.php?nacionalidad={nacionalidad.upper()}&cedula={ci}').read().decode()
 		parsed_html = BeautifulSoup(web, 'html.parser')
 		td = parsed_html.find_all('td', attrs={'align':'left'})
-		return (td[3].getText())
-	except Exception as e:
+		return td[3].getText()
+	except:
 		return None
